937e3fa6e8136d9548229f804f38b000deb712b0
222rohan <rohanshenoy405@gmail.com>
2025-02-05 23:14:02 +0530

    minor bug fixes

--- server/server.h
+++ server/server.h
@@ -11,21 +11,3 @@ extern "C" {
 }
 #endif
 
-
-class ErpoServer {
-public:
-    // Constructor: initializes the server to listen on the specified port
-    ErpoServer(int port);
-
-    // Starts the server loop to accept and handle client connections
-    void start();
-
-private:
-    int port_;
-
-    // Optional: helper function to process an event (can be used within thread handlers)
-    void processEvent(const erpo_event_t &event);
-};
-
-#endif // SERVER_H
-
